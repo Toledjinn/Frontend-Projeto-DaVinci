4933eb6537ea8dce87f082de2202ba03d668fb1f
Luiz Toledo <toled1nn.leal@gmail.com>
2025-09-03 01:37:04 -0300

    Edição de perfil

--- frontend-projeto-davinci/src/screens/app/Profile/ProfileScreen.tsx
+++ frontend-projeto-davinci/src/screens/app/Profile/ProfileScreen.tsx
@@ -3,25 +3,9 @@ import { SafeAreaView, ScrollView, useWindowDimensions } from 'react-native';
 import { useFocusEffect, useRouter } from 'expo-router';
 import { styles } from './ProfileScreen.styles';
 import { useUIStore } from '@/state/uiStore';
-import FotoPerfil from '@/assets/images/FotoPerfil.svg'; 
 import ProfileDataList from '@/components/features/ProfileDataList';
-import ScreenFooter from '@/components/common/ScreenFooter'; 
-
-const MOCK_PROFILE_DATA = [
-  { id: '1', label: 'Gênero', value: 'Masculino' },
-  { id: '2', label: 'Data de Nascimento', value: '05/04/1961' },
-  { id: '3', label: 'Estado Civil', value: 'Solteiro' },
-  { id: '4', label: 'CPF', value: '217.497.441-04' },
-  { id: '5', label: 'CRO', value: '0000 - DF' },
-  { id: '6', label: 'Especialidades', value: 'Periodontia / Prótese' },
-  { id: '7', label: 'Telefone', value: '(61) 98201 - 0910' },
-  { id: '8', label: 'E-mail', value: 'rafael.f.resende@iesb.edu.br' },
-  { id: '9', label: 'CEP', value: '70711-903' },
-  { id: '10', label: 'Endereço', value: 'SCN, Asa Norte Quadra 01 Bloco E' },
-  { id: '11', label: 'Número / Complemento', value: '-' },
-  { id: '12', label: 'Cidade / Estado', value: 'Brasília / DF' },
-  { id: '13', label: 'Nacionalidade', value: 'Brasileira' },
-];
+import ScreenFooter from '@/components/common/ScreenFooter';
+import { findUserById } from '@/data/mockUsers';
 
 export default function ProfileScreen() {
   const { height } = useWindowDimensions();
@@ -29,37 +13,50 @@ export default function ProfileScreen() {
   const setHeaderConfig = useUIStore((state) => state.setHeaderConfig);
   const router = useRouter();
 
+  const user = findUserById('dentist-1');
+
   const handleLogout = () => {
     router.replace('/(auth)/login');
   };
 
   const handleEditData = () => {
-    console.log('Botão "Editar Dados" pressionado');
+     router.push({
+        pathname: '/register',
+        params: { 
+            userType: user?.type, 
+            userId: user?.id 
+        },})
   };
 
   useFocusEffect(
     React.useCallback(() => {
-      setHeaderConfig({
-        layout: 'profile',
-        showBackground: true,
-        showNotificationIcon: false,
-        userName: 'José Maria Gratone', 
-        UserImageSvg: FotoPerfil,   
-      });
-    }, [])
+      if (user) {
+        setHeaderConfig({
+          layout: 'profile',
+          showBackground: true,
+          showNotificationIcon: false,
+          userName: user.name,
+          UserImageSvg: user.image,
+        });
+      }
+    }, [user])
   );
+
+  if (!user) {
+    return null;
+  }
 
   return (
     <SafeAreaView style={styles.safeArea}>
-      <ScrollView 
+      <ScrollView
         style={styles.scrollView}
         contentContainerStyle={[styles.contentContainer, { paddingTop: headerHeight + 9 }]}
         showsVerticalScrollIndicator={false}
       >
-        <ProfileDataList data={MOCK_PROFILE_DATA} />
+        <ProfileDataList data={user.details} />
       </ScrollView>
 
-      <ScreenFooter 
+      <ScreenFooter
         secondaryButtonTitle="Editar Dados"
         onSecondaryButtonPress={handleEditData}
         primaryButtonTitle="Sair"
